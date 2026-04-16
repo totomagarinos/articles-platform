a6a7b053e631ee845e9ba6a2414d3933c2840dde
Juan Cruz Magariños <totomagarinos@gmail.com>
2026-04-16 11:34:36 -0300

    Refactor build script to remove unnecessary --no-input flags for Tailwind installation and building steps, streamlining the deployment process.

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-# Salir inmediatamente si un comando falla
 set -o errexit
 
 # 1. Instalar dependencias de Python
 pip install -r requirements.txt
 
 # 2. Construir los estilos de Tailwind
-python manage.py tailwind install --no-input
-python manage.py tailwind build --no-input
+python manage.py tailwind install
+python manage.py tailwind build
 
 # 3. Recolectar estáticos
 python manage.py collectstatic --noinput
