0100cdad75fdf02a18279b51350e0b79b2dc148e
Juan Cruz Magariños <totomagarinos@gmail.com>
2026-04-16 11:07:18 -0300

    Update static file storage backend in settings.py to use CompressedStaticFilesStorage for improved performance.

--- config/settings.py
+++ config/settings.py
@@ -135,7 +135,7 @@ STORAGES = {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
