6bd3ccbbf4c4f98495fc37a1336e05459ddd6c8c
Juan Cruz Magariños <totomagarinos@gmail.com>
2026-04-17 11:42:31 -0300

    Refactor media file handling in urls.py to serve user-uploaded images in production using Django's serve view, replacing previous static file handling.

--- config/urls.py
+++ config/urls.py
@@ -17,13 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
 ]
 
-# Servir archivos media (imágenes subido por usuarios)
-# En producción DEBUG=False, pero necesitamos servir las imágenes
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Servir archivos media en producción
+urlpatterns += [
+    path('media/<path:path>', serve, {'document_root': settings.MEDIA_ROOT}),
+]
